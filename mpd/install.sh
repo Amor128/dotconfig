5d71fe90e5d034e83156205cf5768a1d83dac9f5
Ermao <yangshifeng19@qq.com>
2024-02-03 00:40:57 +0800

    temp: 20240203T0040

---
+++ mpd/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ~/.config/mpd
+
+cp mpd.conf ~/.config/mpd
+
+# install font JetBrainsMonoNerdFont-Regular.ttf
+mkdir -p ~/.local/share/mpd
+mkdir -p ~/.local/share/mpd/playlists
